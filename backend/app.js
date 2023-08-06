6c9542f945678c8342b902ce11f0dcdd32f77b23
Umar Tariq <umart823@gmail.com>
2023-08-06 23:49:55 +0500

    work started with imagekit

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,25 @@ const connectToMongoDBAtlas = require("./utils/DBconnection");
 require("dotenv").config();
 const cookieParser = require("cookie-parser");
 const jwt = require("jsonwebtoken");
+const ImageKit = require("imagekit");
+const fs = require("fs");
+
+// const imageFile = fs.readFileSync("./uploads/main.jpg");
+// var imagekit = new ImageKit({
+//   publicKey: process.env.IMAGEKIT_PUBLIC_KEY,
+//   privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
+//   urlEndpoint: "https://ik.imagekit.io/umartariq/",
+// });
+// imagekit.upload(
+//   { file: imageFile, fileName: "main.jpg", folder: "try" },
+//   (error, result) => {
+//     if (error) {
+//       console.log(error);
+//     } else {
+//       console.log("Image uploaded successfully.\n" + result.url);
+//     }
+//   }
+// );
 
 const register = require("./routes/register");
 const login = require("./routes/login");
