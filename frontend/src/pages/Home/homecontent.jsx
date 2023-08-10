1d041b8ac229e639557b220b4771a21672e56bba
umart823 <umart823@gmail.com>
2023-08-10 09:58:07 +0500

    optimized animation on homePage

--- frontend/src/pages/Home/homecontent.jsx
+++ frontend/src/pages/Home/homecontent.jsx
@@ -32,7 +32,7 @@ export default function HomeContent() {
           <div className="row h-100">
             <div className="col-md-8 mb-3 text-center d-flex d-sm-flex d-md-flex justify-content-center align-items-center justify-content-md-start align-items-md-center justify-content-xl-center">
               <div style={{ maxWidth: "600px" }}>
-                <div className={styles.ani1}>
+                <div className={`${styles.ani1} ${styles.animatedParagraph}`}>
                   <h1
                     className="text-uppercase text-center fw-bold"
                     style={{ color: "aliceblue" ,fontFamily:'Titillium Web, sans-serif',fontSize: "285%" }}
@@ -40,12 +40,12 @@ export default function HomeContent() {
                     Bringing farm
                   </h1>
                 </div>
-                <div className={styles.ani2}>
+                <div className={`${styles.ani2} ${styles.animatedParagraph}`}>
                   <h1 style={{ color: "aliceblue",fontFamily:'Titillium Web, sans-serif' ,fontSize: "200%" }}>
                     To Your Doorstep And
                   </h1>
                 </div>
-                <div className={styles.ani3}>
+                <div className={`${styles.ani3} ${styles.animatedParagraph}`}>
                   <h1
                     className="text-capitalize mb-5"
                     style={{ color: "aliceblue",fontFamily:'Titillium Web, sans-serif', fontSize: "190%" }}
@@ -53,12 +53,12 @@ export default function HomeContent() {
                     Neighbourhood stores
                   </h1>
                 </div>
-                <div className={styles.ani4}>
+                <div className={`${styles.ani4} ${styles.animatedParagraph}`}>
                   <p style={{ color: "aliceblue",fontFamily:'Titillium Web, sans-serif' ,fontSize: "130%" }}>
                     We are a team of passionate individuals who are committed to bridge the gap between farmers and merket, empowering both. {/* them to reach wider audience and ensure fair prices */}{/*  Our mission is to bridge the gap between farmers and market. */}
                   </p>
                 </div>
-                <div className={styles.ani5}>
+                <div className={`${styles.ani5} ${styles.animatedParagraph}`}>
                   <Link
                     to="/signup"
                     style={{
